776ceb97ab24e7df5a4d8b4079490444f71c6cd1
José Luis Di Biase <josx@interorganic.com.ar>
2021-04-13 15:17:23 -0300

    Editor first version
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- src/index.js
+++ src/index.js
@@ -3,15 +3,37 @@ import { render } from "react-dom";
 import { Editor } from "./lib";
 
 const App = () => {
+  const style = {
+     lineHeight: 2,
+     fontSize: 'large',
+     whiteSpace: 'pre-line',
+     overflowX: 'hidden',
+     overflowY: 'scroll',
+     backgroundColor: 'var(--contrast-color)',
+     color: 'var(--secondary-color)',
+     borderBottom: 'solid',
+     borderColor: 'var(--secondary-color)',
+     borderWidth: '0.1em',
+     height: '100%',
+     padding: '3em',
+     fontFamily: 'Saira-Regular',
+     zoom: 1,
+  }
+  const tags = [
+    { id: 1, name: "PERSON", description: "Persona", should_anonimyzation: true, enable_multiple_selection: false },
+    { id: 2, name: "LOC", description: "Persona", should_anonimyzation: false, enable_multiple_selection: false }
+  ];
+  const annotations = [
+    { id:1, start:18, end:28, tag:'PERSON', should_anonymized: true, human_marked_ocurrency: false }
+  ];
+  const text = "On Monday night , Mr. Fallon will have a co-host for the first time : The rapper Cardi B , who just released her first album,  Invasion of Privacy";
   return (
-    <div style={{ width: 640, margin: "15px auto" }}>
-       <div>
-        <Editor
-           value={[{start:18, end:28, tag:'PERSON'}]}
-           tag="PERSON"
-         />
-      </div>
-    </div>
+      <Editor
+        style={style}
+        annotations={annotations}
+        tags={tags}
+        text={text}
+      />
   );
 }
 
--- src/lib/Editor.js
+++ src/lib/Editor.js
@@ -1,45 +1,311 @@
 import { TextAnnotator } from 'react-text-annotate';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import { Provider, useSelector, useDispatch } from 'react-redux';
+import { combineReducers } from 'redux';
+// eslint-disable-next-line import/no-cycle
+import anonymizerReducer from './anonymizerSlice';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import Instructions from './Instructions';
+import styles from './EditionStep.module.css';
+import {
+  selectAnonymizer,
+  updateAnnotations,
+  updateNewAnnotations,
+  updateDeleteAnnotations,
+  removeNewAnnotations,
+  removeDeleteAnnotations,
+  updateSelectTag,
+  updateTags,
+  updateAnalysisSuccess,
+} from './anonymizerSlice';
 
-const TEXT = "On Monday night , Mr. Fallon will have a co-host for the first time : The rapper Cardi B , who just released her first album,  Invasion of Privacy"
+import {
+  Select,
+  FormControl,
+  MenuItem,
+  Box,
+  createStyles,
+  makeStyles,
+  Paper,
+  Typography,
+} from '@material-ui/core';
 
-const TAG_COLORS = {
-  ORG: '#00ffa2',
-  PERSON: '#84d2ff',
+const createRootReducer = () => {
+  return combineReducers({
+      anonymizer: anonymizerReducer,
+  });
 }
 
-const Editor = ({ value, tag}) => {
-  const [state, setState] = useState({ value, tag })
+const rootReducer = createRootReducer();
+const middleware = [...getDefaultMiddleware()];
+
+export const configuredStore = (initialState) => {
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware,
+    preloadedState: initialState,
+  });
+
+  return store;
+};
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      minWidth: theme.spacing(85),
+      [theme.breakpoints.up('lg')]: {
+        marginRight: theme.spacing(40),
+        marginLeft: theme.spacing(40),
+      },
+      [theme.breakpoints.down('lg')]: {
+        marginRight: theme.spacing(20),
+        marginLeft: theme.spacing(20),
+      },
+      [theme.breakpoints.down('md')]: {
+        marginRight: theme.spacing(8),
+        marginLeft: theme.spacing(8),
+      },
+      [theme.breakpoints.down('sm')]: {
+        marginRight: theme.spacing(0),
+        marginLeft: theme.spacing(0),
+      },
+    },
+    selectInput: {
+      display: 'flex',
+      flexDirection: 'row',
+      backgroundColor: theme.palette.common.white,
+      borderRadius: theme.spacing(10),
+      color: theme.palette.secondary.main,
+      padding: theme.spacing(1, 3),
+      fontSize: 'medium',
+      fontWeight: 'bold',
+      '&:hover, &:focus': {
+        color: theme.palette.primary.main,
+        borderRadius: theme.spacing(10),
+      },
+    },
+    selectIcon: {
+      color: theme.palette.primary.main,
+      paddingRight: theme.spacing(2),
+    },
+    selector: {
+      [theme.breakpoints.down('lg')]: {
+        width: theme.spacing(30),
+      },
+      [theme.breakpoints.up('lg')]: {
+        width: theme.spacing(50),
+      },
+    },
+    tagDescription: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      whiteSpace: 'nowrap',
+      textOverflow: 'ellipsis',
+      overflow: 'hidden',
+    },
+    tagTitle: {
+      marginRight: theme.spacing(1),
+    },
+    selectorContainer: {
+      flexGrow: 1,
+      marginRight: theme.spacing(2),
+      alignItems: 'center',
+    },
+    selectorIcon: {
+      color: theme.palette.primary.main,
+      paddingRight: theme.spacing(1),
+    },
+    root: {
+      '& > *': {
+        marginBottom: theme.spacing(5),
+        height: theme.spacing(50),
+        [theme.breakpoints.down('lg')]: {
+          height: theme.spacing(23),
+        },
+        [theme.breakpoints.down('md')]: {
+          height: theme.spacing(25),
+        },
+        [theme.breakpoints.down('sm')]: {
+          height: theme.spacing(20),
+        },
+      },
+    },
+  })
+);
+
+
+const Editor = ({ style, annotations, tags, text}) => {
+  const state = useSelector(selectAnonymizer);
+  const dispatch = useDispatch();
+  const classes = useStyles();
+
+  useEffect(() => {
+      dispatch(updateTags(tags));
+      const annotationsMap = annotations.map((ent) => {
+          return {
+            ...ent,
+            class: ent.should_anonymized ? styles.anonymousmark : styles.mark,
+          };
+        })
+      dispatch(updateAnalysisSuccess({ents: annotationsMap, text: text}));
+      dispatch(updateSelectTag(tags[0].name));
+  }, [dispatch, annotations, tags, text]);
+
+  const [selectedTag, setSelectedTag] = useState(state.selectTag?.name);
+
+  const renderSelect = () => {
+    return (
+      <FormControl className={classes.selectorContainer} color="secondary">
+        <Select
+          label="TAG"
+          fullWidth
+          labelId="tag"
+          id="tag"
+          value={selectedTag}
+          onChange={(event) => handleTagSelection(event)}
+          className={classes.selector}
+          color="secondary"
+          classes={{ icon: classes.selectorIcon, select: classes.selectInput }}
+          disableUnderline
+          style={{display: 'flex', alignSelf: 'flex-end', margin: '24px'}}
+        >
+          {state.tags.map((tag) => {
+            return (
+              <MenuItem key={tag.id} value={tag.name} id={tag.id}>
+                <Typography
+                  component="h1"
+                  variant="subtitle1"
+                  className={classes.tagTitle}
+                >
+                  {tag.name}
+                </Typography>
+                <Typography
+                  component="h1"
+                  variant="subtitle1"
+                  className={classes.tagDescription}
+                >
+                  {tag.description}
+                </Typography>
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
+    );
+  };
+
+
+  const handleEntitySelection = (value, span) => {
+    // Check if annotations exist in deleteAnnotations array
+    if (
+      state.deleteAnnotations.some(
+        (annot) => annot.start === span.start && annot.end === span.end
+      )
+    ) {
+      // Remove a delete annotation and update annotations by show in editor
+      dispatch(removeDeleteAnnotations(span));
+      dispatch(updateAnnotations([...state.annotations.concat([span])]));
+    } else {
+      // Update new annotations by show in editor
+      dispatch(updateNewAnnotations([span]));
+    }
+  };
+
+  const handleDelete = (index: number, value) => {
+    // Check if annotations exist in newAnnotations array
+    if (
+      state.newAnnotations.some(
+        (annot) => annot.start === value.start && annot.end === value.end
+      )
+    ) {
+      dispatch(removeNewAnnotations(value));
+    } else {
+      // Add a delete annotation and update annotations by don't show in editor
+      dispatch(updateDeleteAnnotations([state.annotations[index]]));
+      dispatch(
+        updateAnnotations([
+          ...state.annotations.slice(0, index),
+          ...state.annotations.slice(index + 1),
+        ])
+      );
+    }
+    return null;
+  };
+
+  const handleClick = (index: number, value) => {
+    handleDelete(index, value);
+  };
+
+  const handleTagSelection = (event) => {
+    dispatch(updateSelectTag(event.target.value));
+    setSelectedTag(event.target.value);
+  };
+
 
   return (
-    <div>
-    <select
-      onChange={e => setState(prevState => ({...prevState, tag: e.target.value }))}
-      value={state.tag}
-    >
-      <option value="ORG">ORG</option>
-      <option value="PERSON">PERSON</option>
-    </select>
-
-    <TextAnnotator
-      style={{
-        maxWidth: 500,
-        lineHeight: 1.5,
-      }}
-      content={TEXT}
-      value={state.value}
-      onChange={value => setState({ value })}
-      getSpan={span => ({
-        ...span,
-        tag: state.tag,
-        color: TAG_COLORS[state.tag],
-      })}
-    />
-    <pre style={{ fontSize: 12, lineHeight: 1.2 }}>
-      {JSON.stringify(state, null, 2)}
-    </pre>
-    </div>
+    <Box>
+      <div className={classes.container}>
+        <Instructions
+          title="Selecciona una etiqueta"
+          subtitle="Luego elimina, agrega o corrige las entidades identificadas. IA² detecta las etiquetas y realizará las siguientes acciones con cada una según su color:"
+          // Hardcodeados los colores y los textos, pensar si no hacer un servicio de backend,que brinde los colores y los textos
+          legends={[
+            {
+              color: '#00D6A1',
+              description: 'Anonimizar',
+            },
+            {
+              color: '#ffca00',
+              description: 'No anonimizar',
+            },
+          ]}
+        >
+          {renderSelect()}
+        </Instructions>
+        <div className={classes.root}>
+          <Paper elevation={5}>
+            <TextAnnotator
+              editableContent
+              doubleTaggingOff
+              style={style}
+              content={state.text}
+              value={state.annotations.concat(state.newAnnotations)}
+              onChange={(value, span) => handleEntitySelection(value, span)}
+              getSpan={(span) => ({
+                ...span,
+                should_anonymized: state.selectTag.should_anonimyzation,
+                human_marked_ocurrency: true,
+                tag: selectedTag,
+                class: state.selectTag.should_anonimyzation
+                  ? styles.anonymousmark
+                  : styles.mark,
+              })}
+              markStyle={styles}
+              markClass={styles.mark}
+              handleClick={(index, value) => handleClick(index, value)}
+              withCompletedWordSelection
+            />
+          </Paper>
+        </div>
+      </div>
+    </Box>
   )
 }
 
-export default Editor;
+const WrappedEditor = ({style, annotations, tags, text}) => {
+  const store = configuredStore();
+  return (
+    <Provider store={store}>
+      <Editor
+        style={style}
+        text={text}
+        tags={tags}
+        annotations={annotations}
+      />
+    </Provider>
+  )
+}
+
+export default WrappedEditor;
